54b1abbdb7423ccda468d5fc69d41ce15278cc35
knightfury16 <suhaibknight@gmail.com>
2022-07-03 00:21:12 +0600

    graphql Array type.

--- src/index.js
+++ src/index.js
@@ -20,16 +20,39 @@ const users = [
   }
 ];
 
+const posts = [
+  {
+    id: 1,
+    title: 'Chartreux',
+    body: 'My dog',
+    published: true
+  },
+  {
+    id: 2,
+    title: 'Paisley Terrier',
+    body: 'My dot',
+    published: false
+  },
+  {
+    id: 3,
+    title: 'Armenian Gampr dog',
+    body: 'My dog',
+    published: true
+  }
+];
 // Schema
 const typeDefs = gql`
   type Query {
     "Returns an array of all users"
-    users: [User!]!
+    users(query: String): [User!]!
 
     "Returns user profile"
     me: User! #Returns user profile
     "Returns a post"
     post: Post!
+
+    "Returns all the post"
+    posts(query: String): [Post!]!
   }
   type User {
     id: ID!
@@ -49,8 +72,23 @@ const typeDefs = gql`
 // Resolver
 const resolvers = {
   Query: {
-    users: () => users,
+    users: (parent, args, ctx, info) => {
+      if (!args.query) return users;
 
+      return users.filter(user => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
+
+    posts: (parent, args, ctx, info) => {
+      if (!args.query) return posts;
+
+      return posts.filter(post => {
+        const isTitleMtch = post.title.toLowerCase().includes(args.query.toLowerCase());
+        const isBodyMtch = post.body.toLowerCase().includes(args.query.toLowerCase());
+        return isTitleMtch || isBodyMtch;
+      });
+    },
     me: () => ({
       id: 'abc123',
       name: 'Mike',
