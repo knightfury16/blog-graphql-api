daafd6bc7fae4e552008e81fdbfeeaf34f511b8f
knightfury16 <suhaibknight@gmail.com>
2022-07-03 20:15:20 +0600

    graphql Mutation done!

--- src/index.js
+++ src/index.js
@@ -1,73 +1,75 @@
 import { ApolloServer, gql } from 'apollo-server';
+import { v4 as uuidv4 } from 'uuid';
+
 // Dummy Users
-const users = [
+let users = [
   {
-    id: 1,
+    id: '1',
     name: 'Dev_Mishra',
     email: 'Aanand.Nehru@example.net',
     age: 22
   },
   {
-    id: 2,
+    id: '2',
     name: 'Agastya_Adiga',
     email: 'Chitramala21@example.net',
     age: 23
   },
   {
-    id: 3,
+    id: '3',
     name: 'Agniprava48',
     email: 'Anasuya.Khatri22@example.com'
   }
 ];
 
-const posts = [
+let posts = [
   {
-    id: 1,
+    id: '1',
     title: 'Learn Apple',
     body: 'Apple is good for dog',
     published: true,
-    author: 1
+    author: '1'
   },
   {
-    id: 2,
+    id: '2',
     title: 'Ball',
     body: 'New type of ball game',
     published: false,
-    author: 3
+    author: '3'
   },
   {
-    id: 3,
+    id: '3',
     title: 'Cat',
     body: 'Cat always land on feet',
     published: true,
-    author: 1
+    author: '1'
   }
 ];
 
-const comments = [
+let comments = [
   {
-    id: 12,
+    id: '12',
     text: 'Wow!',
-    author: 1,
-    post: 1
+    author: '1',
+    post: '1'
   },
   {
-    id: 31,
+    id: '31',
     text: 'Nice Dog',
-    author: 2,
-    post: 3
+    author: '2',
+    post: '3'
   },
   {
-    id: 44,
+    id: '44',
     text: 'Does he bite',
-    author: 1,
-    post: 2
+    author: '2',
+    post: '2'
   },
   {
-    id: 11,
+    id: '11',
     text: 'Cute!',
-    author: 2,
-    post: 1
+    author: '1',
+    post: '2'
   }
 ];
 // Schema
@@ -87,6 +89,34 @@ const typeDefs = gql`
     "returns all the comments"
     comments: [Comment!]!
   }
+  type Mutation {
+    createUser(data: CreateUserInput!): User!
+    deleteUser(id: ID!): User!
+    createPost(data: CreatePostInput!): Post!
+    deletePost(id: ID!): Post!
+    createComment(data: CreateCommentInput!): Comment!
+    deleteComment(id: ID!): Comment!
+  }
+
+  input CreateUserInput {
+    name: String!
+    email: String!
+    age: Int
+  }
+
+  input CreatePostInput {
+    title: String!
+    body: String!
+    published: Boolean!
+    author: ID!
+  }
+
+  input CreateCommentInput {
+    text: String!
+    author: ID!
+    post: ID!
+  }
+
   type User {
     id: ID!
     name: String!
@@ -148,6 +178,93 @@ const resolvers = {
       body: 'Body of my first blog',
       published: true
     })
+  },
+  Mutation: {
+    createUser: (parent, args) => {
+      const isEmailTaken = users.some(user => user.email === args.data.email);
+      // if (isEmailTaken) throw new Error('Email Taken!!');
+
+      const user = {
+        id: uuidv4(),
+        ...args.data
+      };
+
+      users.push(user);
+
+      return user;
+    },
+    deleteUser: (_, args) => {
+      const userIndex = users.findIndex(user => user.id === args.id);
+
+      if (userIndex == -1) throw new Error('User not found!');
+
+      posts = posts.filter(post => {
+        const match = post.author === args.id;
+
+        if (match) {
+          comments = comments.filter(comment => comment.post != post.id);
+        }
+        return !match;
+      });
+
+      comments = comments.filter(comment => comment.author != args.id);
+
+      const deletedUsers = users.splice(userIndex, 1);
+
+      return deletedUsers[0];
+    },
+    createPost: (parent, args) => {
+      const userExits = users.some(user => user.id === args.data.author);
+
+      if (!userExits) throw new Error('User Not Found!');
+
+      const post = {
+        id: uuidv4(),
+        ...args.data
+      };
+
+      posts.push(post);
+
+      return post;
+    },
+    deletePost: (_, args) => {
+      const postIndex = posts.findIndex(post => post.id === args.id);
+
+      if (postIndex == -1) throw new Error('Post not found!');
+
+      comments = comments.filter(comment => comment.post !== args.id);
+
+      const deletedPosts = posts.splice(postIndex, 1);
+
+      return deletedPosts[0];
+    },
+    createComment: (_, args) => {
+      const userExits = users.some(user => user.id === args.data.author);
+      if (!userExits) throw new Error('User not found!');
+
+      const postExits = posts.find(post => post.id === args.data.post);
+      if (!postExits) throw new Error('Post not found!');
+
+      if (!postExits.published) throw new Error('Post not published!');
+
+      const comment = {
+        id: uuidv4(),
+        ...args.data
+      };
+
+      comments.push(comment);
+
+      return comment;
+    },
+    deleteComment: (_, args) => {
+      const commentIndex = comments.findIndex(comment => comment.id === args.id);
+
+      if (commentIndex === -1) throw new Error('Comment not found!');
+
+      const deletedComments = comments.splice(commentIndex, 1);
+
+      return deletedComments[0];
+    }
   },
   Post: {
     author: (parent, args, ctx, info) => {
