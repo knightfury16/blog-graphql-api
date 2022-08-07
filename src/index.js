ac544a4a14fa28a30a4918839d1b163c1185b1da
knightfury16 <suhaibknight@gmail.com>
2022-08-07 21:55:01 +0600

    added morgan for logging.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { scemaWithResolver } from './schema';
 import { prismaSelect } from './utils/prismaSelect';
+import morgan from 'morgan';
 
 const main = async () => {
   const app = express();
+  app.use(morgan('dev'));
 
   const server = new ApolloServer({
     schema: scemaWithResolver,
