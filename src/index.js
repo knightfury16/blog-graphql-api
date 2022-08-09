9ec095d429a58d42c81786895ee555d8e5fcb8b0
knightfury16 <suhaibknight@gmail.com>
2022-08-10 04:47:37 +0600

    added apollo sandbox to the cors origin.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ const main = async () => {
   const app = express();
   app.use(
     cors({
-      origin: 'http://localhost:3000',
+      origin: ['http://localhost:3000', 'https://studio.apollographql.com'],
+
       credentials: true
     })
   );
